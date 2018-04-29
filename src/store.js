2dee96bc3fd83b2d6241fdb7554970372eb74b29
Jacob Kittley-Davies <jacob@kittley.com>
2018-04-29 15:04:39 +0100

    BLE added

--- src/store.js
+++ src/store.js
@@ -3,29 +3,37 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const makePrinter = function (id, name, isVirtual = false) {
+  return {
+    name: 'Printer ' + id,
+    isConnecting: false,
+    id: id,
+    isVirtual: isVirtual
+  }
+}
+
 const modulePrinter = {
   state: {
     selected: null,
-    printers: [{
-      name: 'Virtual Printer',
-      id: 0
-    }]
+    printers: [makePrinter(0, 'Virtual Printer', true)],
+    bleAvailable: false
   },
   mutations: {
     refreshPrinterList (state) {
-      state.printers.push({
-        key: state.printers.length,
-        name: 'Printer ' + state.printers.length,
-        id: 0
-      })
+      state.printers.push(makePrinter(state.printers.length, 'Printer ' + state.printers.length))
     },
     setSelectedPrinter (state, printer) {
       console.log('Setting selected to: ', printer.name)
+      printer.isConnecting = false
       state.selected = printer
     },
     unsetSelectedPrinter (state) {
       console.log('Un Setting selected')
       state.selected = null
+    },
+    setBleAvailable (state, value) {
+      console.log('Setting BLE available to: ', value)
+      state.bleAvailable = value
     }
   },
   actions: {
@@ -33,18 +41,41 @@ const modulePrinter = {
       commit('refreshPrinterList')
     },
     connectToPrinter ({ commit }, printer) {
-      commit('setSelectedPrinter', printer)
+      printer.isConnecting = true
+      setTimeout(function () {
+        commit('setSelectedPrinter', printer)
+      }, 2000)
     },
-    disconnectSelectedPrinter ({ commit }) {
+    disconnectPrinter ({ commit }) {
       commit('unsetSelectedPrinter')
+    },
+    testBLEConnection ({ commit }) {
+      console.log('Testing if BLE radio is available')
+      try {
+        window.ble.isEnabled(
+        function () {
+          commit('setBleAvailable', true)
+        },
+        function () {
+          commit('setBleAvailable', false)
+        })
+      } catch (e) {
+        commit('setBleAvailable', false)
+      }
     }
   },
   getters: {
-    allPrinters: state => {
+    availablePrinters: state => {
       return state.printers
     },
-    getSelectedPrinter: state => {
+    getPrinter: state => {
       return state.selected
+    },
+    hasPrinter: state => {
+      return state.selected !== null
+    },
+    isBleAvailable: state => {
+      return state.bleAvailable
     }
   }
 }
