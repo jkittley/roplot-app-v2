8ae4cdc22ef9e307cf62cd756b927140b53e8812
Jacob Kittley-Davies <jacob@kittley.com>
2018-05-02 10:31:28 +0100

    Show connecting status

--- src/components/PleaseConnect.vue
+++ src/components/PleaseConnect.vue
@@ -3,15 +3,19 @@
     <div class="header" style="margin:50px;">
         <img src="../assets/logo-w-text.svg">
     </div>
-    <v-ons-card class="header">
+    <v-ons-card class="header" @click="$store.commit('splitter/toggle')">
       <v-ons-icon size="50px" icon="ion-printer"></v-ons-icon>
-      <h1>Please connect to a printer.</h1>
+      <h1 v-if="!isConnecting">Please connect to a printer.</h1>
+      <h1 v-else><v-ons-icon icon="fa-refresh" spin></v-ons-icon> Connecting...</h1>
     </v-ons-card>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'please-connect'
+  name: 'please-connect',
+  computed: mapGetters(['isConnecting'])
 }
 </script>
